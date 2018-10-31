3cc7a3ba1583e60aff016d50b888849e04d3ddab
Tyler Witten <turtlewit@gmail.com>
2018-10-31 11:55:19 -0700

    initial commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	bool isTouchingGround = false;
+	bool wantsToJump = false;
+	Rigidbody2D rb; 
+	public float speed;
+	public float jumpForce;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		wantsToJump = Input.GetButton("Jump");
+	}
+
+	void FixedUpdate () {
+		if (isTouchingGround) {
+			if (wantsToJump){
+				rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+				wantsToJump = false;
+			}
+		}
+		rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.tag == "Ground") {
+			isTouchingGround = true;
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if (other.tag == "Ground") {
+			isTouchingGround = false;
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# Programming-Workshop-4
+
+Hello, VGDC Programmers! Before the workshop, please clone this repo:  
+* `git clone https://github.com/VGDC-UCI/Programming-Workshop-4.git` 
+* Or use Github Desktop / another git frontend
+
+Or download a zip file  
+* Clone or Download -> Download ZIP
